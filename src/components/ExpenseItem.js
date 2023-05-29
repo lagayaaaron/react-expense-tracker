e43ee4865143074b4a09c36a0cc4da2e294d46c0
lagayaaaron <lagayaaaron@gmail.com>
2023-05-29 13:17:27 +0800

    Splitting Components Into Multiple Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from "./ExpressDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(data) {
-	const month = data.date.toLocaleString("en-US", { month: "long" });
-	const day = data.date.toLocaleString("en-US", { day: "2-digit" });
-	const year = data.date.getFullYear();
-
 	return (
 		<div className="expense-item">
-			<div>
-				<div>{month}</div>
-				<div>{day}</div>
-				<div>{year}</div>
-			</div>
+            <ExpenseDate date={data.date} />
 			<div className="expense-item__description">
 				<h2>{data.title}</h2>
 				<div className="expense-item__price">{data.amount}</div>
