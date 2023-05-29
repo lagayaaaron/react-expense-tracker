4a9963e2d7f8174ebe90701b9f92dd48f5c97ed4
lagayaaaron <lagayaaaron@gmail.com>
2023-05-29 12:06:23 +0800

    Pass data using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    return (
-        <div className='expense-item'>
-            <div>Date</div>
-            <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
-            </div>
-        </div>
-    );
+function ExpenseItem(data) {
+
+	return (
+		<div className="expense-item">
+			<div>{data.date.toISOString()}</div>
+			<div className="expense-item__description">
+				<h2>{data.title}</h2>
+				<div className="expense-item__price">{data.amount}</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
